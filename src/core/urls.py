98a810f6e54fbe5a16ba1dfeb3aa7b5f7c1d8ac3
Anselm Powell <Anselm.Powell@sport.wales>
2025-02-25 02:24:22 +0000

    version one deploy 7

--- src/core/urls.py
+++ src/core/urls.py
@@ -3,9 +3,8 @@ from django.urls import path, include
 from django.views.generic import RedirectView
 
 urlpatterns = [
-    path('', RedirectView.as_view(url='/api/', permanent=False)),
+    # path('', RedirectView.as_view(url='/api/', permanent=False)),
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),
     path('api/research/', include('research_assistant.urls')),
     path('api/auth/', include('auth_api.urls')),
 ]
