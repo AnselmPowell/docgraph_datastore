c5f1357f7a13d9ff304decc515ef83a096a9ea8c
Anselm Powell <Anselm.Powell@sport.wales>
2025-02-25 16:23:44 +0000

    version one deployment try 10

--- src/config.py
+++ src/config.py
@@ -8,10 +8,10 @@ IS_PRODUCTION_BACKEND = os.environ.get('IS_PRODUCTION_BACKEND', 'false').lower()
 config = {
     'DEBUG': not IS_PRODUCTION_BACKEND,
     'SECRET_KEY': os.environ.get('DJANGO_SECRET_KEY', 'dev-secret-key'),
-    'ALLOWED_HOSTS': ['*'] if not IS_PRODUCTION_BACKEND else ['https://docgraphapi.up.railway.app/'],
+    'ALLOWED_HOSTS': ['*'] if not IS_PRODUCTION_BACKEND else ['https://docgraphapi.up.railway.app'],
     'CORS_ALLOWED_ORIGINS': [
         'http://localhost:3000',
-        'https://docgraph.up.railway.app/'
+        'https://docgraph.up.railway.app'
     ],
     'DATABASE_URL': os.environ.get('POSTGRES_URL') if IS_PRODUCTION_BACKEND else os.environ.get('POSTGRES_URL_DEV'),
     'STATIC_ROOT': BASE_DIR / 'staticfiles',
