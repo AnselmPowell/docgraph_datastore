38b0ca885d612149c073f4c11f147d42614db6db
Anselm Powell <Anselm.Powell@sport.wales>
2025-03-01 17:39:27 +0000

    version one deployment try 17

--- src/config.py
+++ src/config.py
@@ -15,7 +15,8 @@ config = {
         'http://localhost:3000',
     ],
     'DATABASE_URL': os.environ.get('POSTGRES_URL') if IS_PRODUCTION_BACKEND else os.environ.get('POSTGRES_URL_DEV'),
-    'STATIC_ROOT': BASE_DIR / 'staticfiles',
+    # 'STATIC_ROOT': BASE_DIR / 'staticfiles',
+    'STATIC_ROOT': os.path.join(BASE_DIR, 'staticfiles'),
 }
 
 
