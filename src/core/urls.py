ebdae96a0c57bfcd873b6174b1c435d95c6c6a17
Anselm Powell <Anselm.Powell@sport.wales>
2025-03-03 15:13:18 +0000

    version one deployment try 18

--- src/core/urls.py
+++ src/core/urls.py
@@ -1,10 +1,20 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
+@api_view(['GET'])
+def api_root(request):
+    return Response({"message": "Welcome to the Django API DataStore"})
 
 urlpatterns = [
     # path('', RedirectView.as_view(url='/api/', permanent=False)),
+    path('', api_root, name='api-root'),
     path('admin/', admin.site.urls),
     path('api/research/', include('research_assistant.urls')),
     path('api/auth/', include('auth_api.urls')),
 ]
+
+
